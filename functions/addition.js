6943920058e35611c0bc95a8ae0f07721a7edecb
Cezary Szymański <cezaryszymanski7@gmail.com>
2022-01-13 16:32:57 +0100

    message

--- functions/addition.js
+++ functions/addition.js
@@ -1,10 +1,14 @@
 exports.handler = async function (event, context) {
   let x = parseInt(event.queryStringParameters.x);
   let y = parseInt(event.queryStringParameters.y);
+  responseMessage =
+    x && y
+      ? `Wynik dodawania ${x} i ${y} to ${x + y}`
+      : 'Musisz podać parametr x i y w query';
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: `Wynik dodawania ${x} i ${y} to ${x + y}`,
+      message: responseMessage,
     }),
   };
 };
