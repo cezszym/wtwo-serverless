c59db8db4db448f6204c8e0990659f0bb0478df9
Cezary Szymański <cezaryszymanski7@gmail.com>
2022-01-13 16:27:05 +0100

    adding

--- functions/hello.js
+++ functions/hello.js
@@ -1,6 +1,10 @@
 exports.handler = async function (event, context) {
+  let x = parseInt(event.queryStringParameters.x);
+  let y = parseInt(event.queryStringParameters.y);
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: 'Hello World' }),
+    body: JSON.stringify({
+      message: `Wynik dodawania ${x} i ${y} to ${x + y}`,
+    }),
   };
 };
